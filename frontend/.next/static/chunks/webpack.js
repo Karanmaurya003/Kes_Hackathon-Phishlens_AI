b74c27520ad4b970082f66ae981c27e9a2b57dd2
Karanmaurya003 <karanaim11@gmail.com>
2026-03-17 11:48:00 +0530

    enhanced the fronted, added news and prevention steps when user in not logged in

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "aa0d0abbc6cdb1d8"; }
+/******/ 		__webpack_require__.h = function() { return "a50e056506ae21e9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
